f74838fd70d70f8ea44802238c9c4a5b4ed15a0f
sampsund <78979939+sampsund@users.noreply.github.com>
2023-03-01 10:37:57 -0500

    Create providers.tf

---
+++ Azure/providers.tf
@@ -0,0 +1,14 @@
+#Configure the Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.38.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
